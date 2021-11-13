71c58d7e09ae83d71c1604158ea70f2dfc963aee
cveiga819 <cveiga819@gmail.com>
2021-11-13 14:44:23 +0000

    change the loading icon

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ export default () => {
 
         {movieList.length <= 0 && 
         <div className="loading">
-          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="loading netfix" />
+          <img src="https://www.rchandru.com/images/portfolio/loading.gif" alt="loading netfix" />
         </div>
         }
     </div>
